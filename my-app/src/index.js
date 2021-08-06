5e262a55626956853d00d594f0461f9108abb2cc
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-06 14:22:50 +0100

    stepNumber upadates with eacg move

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -92,6 +92,13 @@ import './index.css';
             xIsNext: !this.state.xIsNext,
         });
     }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
     render() {
         const history = this.state.history;
         const current = history[history.length-1];
