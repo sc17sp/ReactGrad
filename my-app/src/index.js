40719bbbe6d99a77ea0b768eac85ee45b51a1129
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-06 14:01:00 +0100

    Game displays list of previous moves

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -96,7 +96,14 @@ import './index.css';
         const current = history[history.length-1];
         const winner = calculateWinner(current.squares);
   
-        
+        const moves = history.map((step,move)=>{
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li>
+                    <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
         let status;
         if (winner){
             status = 'Winner: ' + winner;
@@ -113,7 +120,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
