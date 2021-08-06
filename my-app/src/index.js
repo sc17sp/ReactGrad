78fab6d3b4e596790c7fa712f00cda5b11bbf5d8
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-06 12:06:12 +0100

    square render displays x onClick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,7 +11,8 @@ class Square extends React.Component {
     }
     render() {
       return (
-        <button className="square" onClick={() => console.log('click')}>
+        <button className="square" 
+                onClick={() => this.setState({value: 'X'})}>
           {this.state.value}
         </button>
       );
