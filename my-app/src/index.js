9438cc8e414a316335f5dfefdc986b9d3aef212c
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-06 12:48:09 +0100

    squares do not update if game won or square already filled

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -38,6 +38,10 @@ import './index.css';
 
     handleClick(i){
         const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({squares: squares,
                        xIsNext: !this.state.xIsNext,
